6cf5ee39adec4dcc05d7f7baf5e93dc1b585f316
Lucian Cardoso <luciancardoso20@gmail.com>
2023-06-15 21:16:56 -0400

    Laço For - Desafio_for_1

---
+++ Fundamentos/ControlandoCodigo/EstruturasDeControle/controle/desafio_for_1.js
@@ -0,0 +1,7 @@
+// 10, 8, 6, 5, 4, 2, 0
+
+for(let contandor = 10; contandor > 0; contandor -= 2){
+    console.log(contandor)
+}
+
+console.log('FIM')
---
+++ Fundamentos/ControlandoCodigo/EstruturasDeControle/controle/for_2.js
@@ -0,0 +1,11 @@
+// Determinada de repetições => x10
+
+for(let numero = 1; numero <= 10; numero++){
+    console.log(numero)
+}
+
+for(let numero = 0; numero < 10; numero++){
+    console.log(numero)
+}
+
+console.log('FIM!')
