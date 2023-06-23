18f48f7d180272cbb102ca68508503fbe84df5ba
Lucian Cardoso <luciancardoso20@gmail.com>
2023-06-22 21:42:17 -0400

    Funcões

---
+++ Fundamentos/Evoluindo-na-Programacao/funcoes/sem_uso_funcao.js
@@ -0,0 +1,11 @@
+const d1 = 7
+const m1 = 'Maio'
+const ano = 2022
+
+console.log(`Dia: ${d1} de ${m1} de ${ano}`)
+
+const d2 = 24
+const m2 = 'Dezembro'
+const a2 = 2024
+
+console.log(`Dia: ${d2} de ${m2} de ${a2}`)
---
+++ Fundamentos/Evoluindo-na-Programacao/funcoes/sem_uso_funcao2.js
@@ -0,0 +1,19 @@
+const d1 = 7
+const m1 = 'Maio'
+const ano = 2022
+
+function exibirData1() {
+    console.log(`Dia: ${d1} de ${m1} de ${ano}`)
+}
+
+exibirData1()
+
+const d2 = 24
+const m2 = 'Dezembro'
+const a2 = 2024
+
+function exibirData2() {
+    console.log(`Dia: ${d2} de ${m2} de ${a2}`)
+}
+
+exibirData2()
