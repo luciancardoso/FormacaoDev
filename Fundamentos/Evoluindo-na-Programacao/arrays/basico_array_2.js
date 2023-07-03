8b40a352f94a24fca7504f86be257b69fa4916b3
Lucian Cardoso <luciancardoso20@gmail.com>
2023-07-03 19:12:30 -0400

    basico array 2

---
+++ Fundamentos/Evoluindo-na-Programacao/arrays/basico_array_2.js
@@ -0,0 +1,14 @@
+const aprovados = []
+
+aprovados[0] = 'Tiago'
+aprovados[1] = 'Ana'
+aprovados[2] = 'Bia'
+aprovados[3] = 'Carlos'
+aprovados[4] = 'Daniel'
+aprovados[5] = 'Helena'
+aprovados[6] = 'Alberto'
+
+console.log(aprovados)
+console.log(aprovados[5])
+
+console.log(typeof aprovados)
