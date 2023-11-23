3fa07dc7b992e4b344452a7d62741d1cf563c279
Lucian Cardoso <luciancardoso20@gmail.com>
2023-11-22 22:08:40 -0400

    Funcão Map

--- Fundamentos/FuncoesEmJavaScript/Map/map_03.js
+++ Fundamentos/FuncoesEmJavaScript/Map/map_03.js
@@ -2,7 +2,14 @@ const gritar = texto => texto.toUpperCase()
 const enfatizar = texto => texto + '!!!'
 const exagerar = texto => texto.split('').join(' ')
 
+const talk = text => text.toUpperCase()
+const user = text => text + '!!!'
+
 const frase = ['Cuidado', 'Olha o carro']
+const falar = ['Bem-vindo', 'usuário']
 
 const final = frase.map(gritar).map(enfatizar).map(exagerar)
 console.log(final)
+
+const fala = falar.map(talk).map(user)
+console.log(fala)
