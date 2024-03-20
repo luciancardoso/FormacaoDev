daa102e46cc61911a6045232b63b14fe1e9e66fe
Lucian Cardoso <luciancardoso20@gmail.com>
2024-03-19 20:17:17 -0400

    Fundamentos de Node - Module exports #02

---
+++ Curso-Node/calculadora-modular/versao2/index.js
@@ -0,0 +1,12 @@
+// const calculos = require('./calculos')
+const {texto, soma, sub} = require("./calculos")
+
+// console.log(calculos)
+
+let x, y;
+x = 10
+y = 5
+
+console.log(soma(x,y))
+console.log(sub(x,y))
+console.log(texto)
