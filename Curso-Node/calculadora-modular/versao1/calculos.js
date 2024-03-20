97e74b6c36a58318f83e3fbc4d54d0ad25fe0f06
Lucian Cardoso <luciancardoso20@gmail.com>
2024-03-19 20:00:34 -0400

    Fundamentos de Node - Module exports #01

---
+++ Curso-Node/calculadora-modular/versao1/calculos.js
@@ -0,0 +1,17 @@
+module.exports = {
+    texto: "Sou um módulo calculo",
+    soma: function(a, b){
+        return a + b
+    },
+    sub: function(a, b){
+        return a - b
+    }
+}
+
+// function soma(a, b){
+//     return a + b
+// }
+
+// function sub(a, b){
+//     return a - b
+// }
---
+++ Curso-Node/calculadora-modular/versao1/index.js
@@ -0,0 +1,11 @@
+const calculos = require('./calculos')
+
+// console.log(calculos)
+
+let x, y;
+x = 10
+y = 5
+
+console.log(calculos.soma(x,y))
+console.log(calculos.sub(x,y))
+console.log(calculos.texto)
