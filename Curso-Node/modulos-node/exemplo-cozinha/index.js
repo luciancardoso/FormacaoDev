9452b86150645efcc232df92526d59a88ac08021
Lucian Cardoso <luciancardoso20@gmail.com>
2024-03-24 21:51:06 -0400

    Fundamentos de Node - Inicializar Módulo - Parte #01

--- Curso-Node/modulos-node/exemplo-cozinha/index.js
+++ Curso-Node/modulos-node/exemplo-cozinha/index.js
@@ -3,6 +3,15 @@
 
 // console.log(acoes.cozinhar(alimentos[0]))
 
-const cozinha = require("./cozinha")
+// const cozinha = require("./cozinha")
+// const receita = require("./receita")
 
-console.log(cozinha)
+// console.log(cozinha)
+// console.log(receita(["Carne", "Alface"], cozinha.cortar, cozinha.assar))
+
+const refeicao = require("./refeicao")("Almoço")
+const refeicao2 = require("./refeicao")("Jantar")
+console.log(refeicao)
+
+refeicao( "arroz", "feijão", "alface")
+refeicao2( "arroz", "feijão", "batata")
