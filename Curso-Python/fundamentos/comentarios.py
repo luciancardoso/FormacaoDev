641c00b34a67a763395990d8b0e3515b3e7f5d64
Lucian Cardoso <luciancardoso20@gmail.com>
2025-06-25 21:35:59 -0300

    Primeitos fundamentos de Python

---
+++ Curso-Python/fundamentos/comentarios.py
@@ -0,0 +1,5 @@
+# A multiplicação tem prioridades
+# O resultado será 13
+print(4 + 3 * 3)
+
+print('O' + 'i' * 10)
---
+++ Curso-Python/fundamentos/primeiro.py
@@ -0,0 +1,3 @@
+print("Lucian")
+print("Hoje está fazenndo 14C")
+print("Hoje está fazendo", 3 * 7, "Graus")
